7b95aa6df2b1d5d6e6d67bbd7e29cbea252e6a25
Tiago Web <tiagosmweb@gmail.com>
2022-06-19 08:49:53 -0400

    component entrada header

---
+++ lib/components/TimeEntry.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class TimeEntry extends StatelessWidget {
+  final String titulo;
+  final int valor;
+
+  const TimeEntry({
+    Key? key,
+    required this.titulo,
+    required this.valor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          titulo,
+          style: const TextStyle(fontSize: 25),
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+                primary: Colors.red,
+              ),
+              child: const Icon(
+                Icons.arrow_downward,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              '$valor min',
+              style: const TextStyle(fontSize: 18),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+                primary: Colors.red,
+              ),
+              child: const Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
